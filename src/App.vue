13880efedc06ffd8bd9c293cad6e02442ed7cd32
rpauli <Ralf.Pauli@edvschule-plattling.de>
2022-10-10 09:31:08 +0200

    reactivly connect values to variables in Allgemeine Infos

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <script setup>
 import {RouterLink, RouterView} from 'vue-router'
-import Sidebar from "@/components/Sidebar.vue";
-import {sidebarWidth} from '@/components/state.js'
 </script>
 
 <template>
